1695b206837ffedc9056c11262c8908137ad21d9
EC2 Default User <ec2-user@ip-172-31-39-41.ec2.internal>
2025-04-14 17:23:21 +0000

    test

--- event_ticketing_project/settings.py
+++ event_ticketing_project/settings.py
@@ -140,6 +140,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
